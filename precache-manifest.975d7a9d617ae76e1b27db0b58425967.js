27ea9e7346fe53846d53b717906082d2f195ee8e
DucDung <bebom098@gmail.com>
2020-06-18 22:39:59 +0700

    Updates

---
+++ precache-manifest.975d7a9d617ae76e1b27db0b58425967.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "396d560bdc8ab4caa59aa0cab76e6c74",
+    "url": "/react-qlnv/index.html"
+  },
+  {
+    "revision": "a876b40faa7529d8c13d",
+    "url": "/react-qlnv/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "63ac77cbf35116b815b4",
+    "url": "/react-qlnv/static/js/2.f1553bfb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-qlnv/static/js/2.f1553bfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a876b40faa7529d8c13d",
+    "url": "/react-qlnv/static/js/main.3905d80f.chunk.js"
+  },
+  {
+    "revision": "5ed03d57873b41d4441b",
+    "url": "/react-qlnv/static/js/runtime-main.7ff9a5c9.js"
+  }
+]);
